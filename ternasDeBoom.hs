d5e458354c910c10c6bd1d7f80d69e17851e98ae
daro <dfabricio@qualyt.com>
2019-03-22 00:01:34 -0300

    inicial

---
+++ ternasDeBoom.hs
@@ -0,0 +1,46 @@
+import Data.Function.Memoize
+main = do  print  (sumarTernas (obtenerTernas [1..1000]))
+           return (sumarTernas (obtenerTernas [1..1000]))
+
+esPrimo :: Integer -> Bool
+esPrimo 1 = False
+esPrimo 2 = True
+esPrimo n = not((length [x | x <- [2 .. n-1], mod n x == 0]) > 0 )
+
+memoizedEsPrimo :: Integer->Bool
+memoizedEsPrimo unNumero 		= memoize esPrimo unNumero	   
+formanPrimo unNumero otroNumero = memoizedEsPrimo (unNumero + otroNumero)
+
+sonTernaDeBoom num1 num2 num3 	= (formanPrimo num1 num2) && (formanPrimo num2 num3)
+esParteDeTernaInferior unNumero = sonTernaDeBoom unNumero (unNumero+1) (unNumero+2) 
+filtrarTernasDeBoom 			= filter esParteDeTernaInferior 
+construirTerna unNumero			= (unNumero,(unNumero+1),(unNumero+2))
+obtenerTernas 					= map construirTerna.filtrarTernasDeBoom
+sumar (x,y,z)					= x+y+z
+sumarTernas 					= map sumar 
+
+
+obtenerPrimosDeTerna (x,y,z)	= (x+y,y+z)
+getPrimosFromTernas 			= (map obtenerPrimosDeTerna ).obtenerTernas 
+
+ternaEsMultiploDe3:: (Integer,Integer,Integer)->Bool
+
+ternaEsMultiploDe3 				= (==0).(flip mod 3).sumar
+
+sonMultiplosDe3:: [(Integer,Integer,Integer)]->Bool
+
+sonMultiplosDe3 				= all ternaEsMultiploDe3
+
+getKMaximo unNumero (otroNumero:numeros)
+ | 3 == unNumero					= 0
+ | (3*otroNumero + 3) == unNumero 	= otroNumero
+ | (3*otroNumero + 3) > unNumero 	= otroNumero-1
+ | otherwise 						= getKMaximo unNumero numeros 
+
+esMultiploDe3k3ConKIgualA3n unNumero	= ((==0). mod (getKMaximo unNumero [1..])) 3
+
+--observaciones: cuando busco los primos del 1 al 100 que no estan en los generados por las ternas me encuentro con todos aquellos primos laterales no gemelos
+--asi que si puedo estudiar el patron que hay en estas ternas puedo obtener la ubicacion de todos los primso gemelos
+--la suma de los numeros de todas las ternas es multiplo de 3 !- duh que gil n + n+1 + n+2 = (3n +3 ) siempre da resto 0
+-- si bien los pares de ternas siempre generan 3k+3, los k= 3 n parecen no ser parte de los generados por las ternas-- chequear esto 
+-- chequear que todas las sumas de ternas se puedan formar con alguna suma 2  ternas menores al numero
